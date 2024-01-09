535f123e2cd268bb26e0e02352bd9f61705a1ee5
Patrick Lehmann <Paebbels@gmail.com>
2024-01-09 14:59:24 +0100

    Bumped version to v0.2.0.

--- sphinx_reports/__init__.py
+++ sphinx_reports/__init__.py
@@ -38,7 +38,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2023-2024, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.1.0"
+__version__ =   "0.2.0"
 __keywords__ =  ["Python3", "Sphinx", "Extension", "Report", "doc-string", "interrogate"]
 
 from typing import Any, Tuple, Dict, Optional as Nullable
