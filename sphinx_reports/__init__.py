ac34000248f501da5c93636e514f2b2f85850f40
Patrick Lehmann <Paebbels@gmail.com>
2026-01-20 20:33:34 +0100

    Bumped version information.

--- sphinx_reports/__init__.py
+++ sphinx_reports/__init__.py
@@ -11,7 +11,7 @@
 #                                                                                                                      #
 # License:                                                                                                             #
 # ==================================================================================================================== #
-# Copyright 2023-2025 Patrick Lehmann - Bötzingen, Germany                                                             #
+# Copyright 2023-2026 Patrick Lehmann - Bötzingen, Germany                                                             #
 #                                                                                                                      #
 # Licensed under the Apache License, Version 2.0 (the "License");                                                      #
 # you may not use this file except in compliance with the License.                                                     #
@@ -41,7 +41,7 @@ Supported reports:
 """
 __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
-__copyright__ = "2023-2025, Patrick Lehmann"
+__copyright__ = "2023-2026, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
 __version__ =   "0.10.0"
 __keywords__ =  [
